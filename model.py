1e3e969ceb4071e8295ba2568cf152b70b6fcfc9
Qichang Dong <krisdong88@gmail.com>
2024-05-08 14:35:52 -0400

    Fully Function YOLO V1 Complete, add comments later

--- model.py
+++ model.py
@@ -90,10 +90,3 @@ class Yolov1(nn.Module):
             nn.LeakyReLU(0.1),
             nn.Linear(496, S* S* (C + B * 5)) # (S, S, 30) C+B*5 = 30
         )
-
-def test(S = 7, B = 2, C = 20):
-    model = Yolov1(split_size = S, num_boxes =B, num_classes = C)
-    x = torch.randn((2,3,448,448))
-    print(model(x).shape)
-
-test()
--- train.py
+++ train.py
@@ -1,3 +1,8 @@
+"""
+Main file for training Yolo model on Pascal VOC dataset
+
+"""
+
 import torch
 import torchvision.transforms as transforms
 import torch.optim as optim
@@ -21,13 +26,12 @@ from loss import YoloLoss
 seed = 123
 torch.manual_seed(seed)
 
-# Hyperparameters etc
-
+# Hyperparameters etc. 
 LEARNING_RATE = 2e-5
-DEVICE = "cude" if torch.cuda.is_available() else "cpu"
-BATCH_SIZE = 16
+DEVICE = "cuda" if torch.cuda.is_available else "cpu"
+BATCH_SIZE = 16 # 64 in original paper but I don't have that much vram, grad accum?
 WEIGHT_DECAY = 0
-EPOCHES = 100
+EPOCHS = 1000
 NUM_WORKERS = 2
 PIN_MEMORY = True
 LOAD_MODEL = False
@@ -43,81 +47,83 @@ class Compose(object):
     def __call__(self, img, bboxes):
         for t in self.transforms:
             img, bboxes = t(img), bboxes
-            return img, bboxes
 
-transform = Compose([transforms.Resize((448,448)),transforms.ToTensor()])
+        return img, bboxes
+
+
+transform = Compose([transforms.Resize((448, 448)), transforms.ToTensor(),])
+
 
 def train_fn(train_loader, model, optimizer, loss_fn):
-    loop = tqdm(train_loader, leave = True)
+    loop = tqdm(train_loader, leave=True)
     mean_loss = []
 
     for batch_idx, (x, y) in enumerate(loop):
-        x, y = x. to(DEVICE), y.to(DEVICE)
+        x, y = x.to(DEVICE), y.to(DEVICE)
         out = model(x)
-        loss = loss.fn(out, y)
+        loss = loss_fn(out, y)
         mean_loss.append(loss.item())
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
-        loop.set_postfix(loss = loss.item())
+        # update progress bar
+        loop.set_postfix(loss=loss.item())
 
     print(f"Mean loss was {sum(mean_loss)/len(mean_loss)}")
 
-def main():
-    model = Yolov1(split_size = 7, num_boxes = 2, num_classes = 20).to(DEVICE)
 
+def main():
+    model = Yolov1(split_size=7, num_boxes=2, num_classes=20).to(DEVICE)
     optimizer = optim.Adam(
         model.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY
     )
-    loss_fn = YoloLoss
+    loss_fn = YoloLoss()
 
     if LOAD_MODEL:
-        load_checkpoint(torch.load(LOAD_MODEL_FILE),model, optimizer)
+        load_checkpoint(torch.load(LOAD_MODEL_FILE), model, optimizer)
 
     train_dataset = VOCDataset(
-        "8examples.csv",
-        transform = transform,
-        img_dir = IMG_DIR,
-        lagbel_dir = LABEL_DIR
+        "data/100examples.csv",
+        transform=transform,
+        img_dir=IMG_DIR,
+        label_dir=LABEL_DIR,
     )
 
     test_dataset = VOCDataset(
-        "test.csv", 
-        transform = transform, 
-        img_dir= IMG_DIR, 
-        label_dir = LABEL_DIR
+        "data/test.csv", transform=transform, img_dir=IMG_DIR, label_dir=LABEL_DIR,
     )
 
     train_loader = DataLoader(
-        dataset = train_dataset,
-        batch_size = BATCH_SIZE,
-        num_workers = NUM_WORKERS,
+        dataset=train_dataset,
+        batch_size=BATCH_SIZE,
+        num_workers=NUM_WORKERS,
         pin_memory=PIN_MEMORY,
-        shuffle = True,
-        drop_last=False
+        shuffle=True,
+        drop_last=True,
     )
 
     test_loader = DataLoader(
-        dataset = test_dataset,
-        batch_size = BATCH_SIZE,
-        num_workers = NUM_WORKERS,
+        dataset=test_dataset,
+        batch_size=BATCH_SIZE,
+        num_workers=NUM_WORKERS,
         pin_memory=PIN_MEMORY,
-        shuffle = True,
-        drop_last=True
+        shuffle=True,
+        drop_last=True,
     )
 
-    for epoch in range(EPOCHES):
+    for epoch in range(EPOCHS):
+
         pred_boxes, target_boxes = get_bboxes(
             train_loader, model, iou_threshold=0.5, threshold=0.4
         )
 
         mean_avg_prec = mean_average_precision(
-            pred_boxes, target_boxes, iou_threshold=0.5, box_format = "midpoint"
+            pred_boxes, target_boxes, iou_threshold=0.5, box_format="midpoint"
         )
+        print(f"Train mAP: {mean_avg_prec}")
 
-        print(f"Train mAp: {mean_avg_prec}")
-        train_fn(train_loader,model,optimizer, loss_fn)
+        train_fn(train_loader, model, optimizer, loss_fn)
 
 
 if __name__ == "__main__":
